316c6106c5fbbbc292a1481defdbac18026db226
Raya0615 <yuktoleung@gmail.com>
2026-08-31 17:47:36 +0800

    UI v0.6.5
    - Updated Trigger Popups
    - Created Boots Timer

--- Assets/_Game/Scripts/UI/HUDCollectiblesController.cs
+++ Assets/_Game/Scripts/UI/HUDCollectiblesController.cs
@@ -1,24 +1,64 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class HUDCollectiblesController : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI eggsCountText;
 
-    void Start()
-    {
+    [SerializeField]
+    private GameObject BootsTimer;
+    [SerializeField]
+    private TextMeshProUGUI TimerText;
 
-    }
+    private Coroutine TimerCoroutine;
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        BootsTimer = GameObject.Find("Boots Timer");
+        TimerText = BootsTimer.GetComponentInChildren<TextMeshProUGUI>();
+
+        // Test
+
+        UpdateBootsTimer(10);
     }
 
     public void SetEggsCount(int count)
     {
         eggsCountText.text = $"{count}";
+    }
+
+    public void UpdateBootsTimer(float duration)
+    {
+        if (TimerCoroutine != null)
+        {
+            StopCoroutine(TimerCoroutine);
+        }
+        TimerCoroutine = StartCoroutine(BootsTimerCountdown(duration));
+    }
+
+    private IEnumerator BootsTimerCountdown(float duration)
+    {
+        float timer = duration;
+        string text;
+        int min;
+        int sec;
+
+        BootsTimer.SetActive(true);
+        while (timer > 0)
+        {
+            min = Mathf.FloorToInt(timer / 60);
+            sec = Mathf.FloorToInt(timer % 60);
+            text = $"{min}:{sec:00}";
+            TimerText.text = text;
+            yield return null; //Waits for next frame
+
+            timer -= Time.deltaTime;
+        }
+        TimerText.text = "0:00";
+        BootsTimer.SetActive(false);
+
+        TimerCoroutine = null;        
     }
 }
